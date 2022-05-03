68a44adc942f591e1c7f03238946ebb31f562c0d
nindozzmo <80521361+nindozzmo@users.noreply.github.com>
2022-05-03 14:21:57 +0300

    Update move.js

--- js/move.js
+++ js/move.js
@@ -4,19 +4,22 @@ window.onload = function () {
   let left = document.querySelector('.left-eye');
   let right = document.querySelector('.right-eye');
   let mouth = document.querySelector('.mouth');
+  
   //for mouse devices
-  wrapper.addEventListener('mousemove', (event) => {
-    
+  wrapper.addEventListener('mousemove', (event) => {    
     let move = (event.clientY * 0.07);
     if (move < 0) {
       move = 0;
     }
+    
     //eyes movement
     left.style.transform = `translateX(-${move}rem)`;
     right.style.transform = `translateX(${move}rem)`;    
+    
     //mouth placeholder
     mouth.innerHTML = ')';
     mouth.style.marginTop = 0;
+    
     //mouth animation  
     if (move > 10 && move < 20) {
       mouth.innerHTML = 'D';
@@ -27,19 +30,22 @@ window.onload = function () {
       mouth.style.marginTop = '3rem';
     }
   })
+  
   //for touch devices
-  wrapper.addEventListener('touchmove', (event) => {
-    
+  wrapper.addEventListener('touchmove', (event) => {    
     let move = (event.touches[0].pageY * 0.07);
     if (move < 0) {
       move = 0;
     }
+    
     //eyes movement
     left.style.transform = `translateX(-${move}rem)`;
     right.style.transform = `translateX(${move}rem)`;    
+    
     //mouth placeholder
     mouth.innerHTML = ')';
     mouth.style.marginTop = 0;
+    
     //mouth animation  
     if (move > 10 && move < 20) {
       mouth.innerHTML = 'D';
@@ -50,7 +56,5 @@ window.onload = function () {
       mouth.style.marginTop = '3rem';
     }
   })
-
   
-
 }
