02daeb381eddb1d14c59c69141f74b50ccbd396b
nindozzmo <80521361+nindozzmo@users.noreply.github.com>
2022-05-03 14:02:35 +0300

    Add files via upload

--- js/move.js
+++ js/move.js
@@ -4,14 +4,16 @@ window.onload = function () {
   let left = document.querySelector('.left-eye');
   let right = document.querySelector('.right-eye');
   let mouth = document.querySelector('.mouth');
-
+  //for mouse devices
   wrapper.addEventListener('mousemove', (event) => {
-    //eyes movement
+    
     let move = (event.clientY * 0.07);
-
+    if (move < 0) {
+      move = 0;
+    }
+    //eyes movement
     left.style.transform = `translateX(-${move}rem)`;
-    right.style.transform = `translateX(${move}rem)`;
-
+    right.style.transform = `translateX(${move}rem)`;    
     //mouth placeholder
     mouth.innerHTML = ')';
     mouth.style.marginTop = 0;
@@ -25,4 +27,30 @@ window.onload = function () {
       mouth.style.marginTop = '3rem';
     }
   })
+  //for touch devices
+  wrapper.addEventListener('touchmove', (event) => {
+    
+    let move = (event.touches[0].pageY * 0.07);
+    if (move < 0) {
+      move = 0;
+    }
+    //eyes movement
+    left.style.transform = `translateX(-${move}rem)`;
+    right.style.transform = `translateX(${move}rem)`;    
+    //mouth placeholder
+    mouth.innerHTML = ')';
+    mouth.style.marginTop = 0;
+    //mouth animation  
+    if (move > 10 && move < 20) {
+      mouth.innerHTML = 'D';
+      mouth.style.marginTop = '2rem';
+    }
+    else if (move > 20) {
+      mouth.innerHTML = 'O';
+      mouth.style.marginTop = '3rem';
+    }
+  })
+
+  
+
 }
